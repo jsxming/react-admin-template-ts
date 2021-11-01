ec878c39373b21e9030fbca2311885b52bc97641
小明同学i <923727421@qq.com>
2021-11-01 17:03:49 +0800

    1、完成动态切换主题色功能

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -3,12 +3,13 @@
  * @Autor: 小明～
  * @Date: 2021-10-21 16:35:13
  * @LastEditors: 小明～
- * @LastEditTime: 2021-10-21 17:29:55
+ * @LastEditTime: 2021-11-01 14:42:59
  */
 
 import React from 'react';
 import Aside from './Aside';
 import Header from './Header';
+import  './main.less';
 
 interface IProps {
     children:React.ReactElement
@@ -18,7 +19,7 @@ export default function LayoutMain(props:IProps){
     return (
         <>
             <Header></Header>
-            <div className="flex-start align-start">
+            <div className="flex-start align-start layout-main">
                 <Aside></Aside>
                 <div className="flex-1">
                     {props.children}
