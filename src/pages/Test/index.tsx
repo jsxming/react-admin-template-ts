55ad6dd922f807cec12bd2c30806f790ff8a1545
田定明 <923727421@qq.com>
2022-11-10 18:12:30 +0800

    feat: 移动端swiper组件添加

--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -1,81 +1,58 @@
-/*
- * @Description:
- * @Autor: 小明～
- * @Date: 2021-09-02 17:31:40
- * @LastEditors: 小明～
- * @LastEditTime: 2021-11-09 09:42:38
- */
-import React, {   useEffect, useRef, useState,createContext,useContext,useMemo } from 'react';
-import { useHistory,Link } from 'react-router-dom';
-// import ReactDOM from 'react-dom';
-import Auth from '@/components/common/Auth';
+import { Cascader } from 'antd';
+import React, { useEffect, useState } from 'react';
+const options = [
+    {
+        value: 'zhejiang',
+        label: 'Zhejiang',
+        children: [
+            {
+                value: 'hangzhou',
+                label: 'Hangzhou',
+                children: [
+                    {
+                        value: 'xihu',
+                        label: 'West Lake',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        value: 'jiangsu',
+        label: 'Jiangsu',
+        children: [
+            {
+                value: 'nanjing',
+                label: 'Nanjing',
+                children: [
+                    {
+                        value: 'zhonghuamen',
+                        label: 'Zhong Hua Men',
+                    },
+                ],
+            },
+        ],
+    },
+];
 
-import {Button} from 'antd';
-// import { JSXElement } from '@babel/types';
-// import '@/ts_study/类型'
-// import '@/ts_study/interface'
 
 
-const TestContext = createContext(1);
-
-function Child(){
-    const test = useContext(TestContext);
-    return <div>
-        <h1>test=={test}</h1>
-    </div>;
-}
-
-function Child2(){
-    return useMemo(() => {
-        return <h1>child22222</h1>;
-    }, []);
-}
-
-export default function Test() {
-    const h = useHistory();
-    const el = useRef(null);
-    const [val, setVal] = useState('1');
-
+const App = () => {
+    const [value, setV] = useState<string[]>([]);
     useEffect(() => {
-        console.log(h);
+        setTimeout(() => {
+            setV(['xihu']);
+        }, 1000);
     }, []);
-
-    const log = (e: React.SyntheticEvent) => {
-        console.log(e.target, e);
-    };
-    const change = (e: React.FormEvent<HTMLInputElement>) => {
-        console.log(e.currentTarget.value);
-        const v = e.currentTarget.value;
-        // let el = e.target as HTMLInputElement
-        // console.log(el.value);
-        // console.log(e.target.value);
-        setVal(v);
+    const onChange = (value: any) => {
+        console.log(value);
     };
 
-    let [count, setCount] = useState(1);
+    return <Cascader onChange={onChange}
+        options={options}
+        placeholder="Please select"
+        value={value} />;
+};
 
-    useEffect(() => {
-        console.log(el.current);
-    }, []);
 
-    return (
-        <div>
-            <h1 onClick={log}
-                ref={el}>back------------------------</h1>
-            <Button onClick={()=>setCount(++count)}
-                type="primary" >fadsfasdf</Button>
-            <Link to="/login" >login</Link>
-            <TestContext.Provider value={count}>
-                <Child/>
-                <Child2/>
-            </TestContext.Provider>
-            <Auth id={14}>
-                <h1>111</h1>
-            </Auth>
-            <Auth id={15}>
-                <h1>222</h1>
-            </Auth>
-        </div>
-    );
-}
-
+export default App;
